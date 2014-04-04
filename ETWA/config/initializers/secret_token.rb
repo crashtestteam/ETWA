a61af67b2c9d8780434aac1377e4ef1750975446
CrashTestTeam <crashtesteam@gmail.com>
2014-04-04 11:36:22 +0300

    commit of the rails project

---
+++ ETWA/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ETWA::Application.config.secret_key_base = '1703cd1b9723645c851788f1a64c50cd43e67b9ce0a635795c3763920a72b53437c312ee9fc1445d04c85026485cd745696ae65d0d7fe82831b74cff645ea719'
