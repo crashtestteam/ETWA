5ed7e7f4874404eae9f348a422cf90a32a6a9896
student <student@lubuntu13.10.planet.zz>
2014-04-08 11:48:54 +0300

    added password field to user

--- EntryApp/db/schema.rb
+++ EntryApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407170334) do
+ActiveRecord::Schema.define(version: 20140408084445) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20140407170334) do
     t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
