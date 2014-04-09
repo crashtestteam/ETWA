a61e1dfa87755d6c3b8e3323ba5a28316f295c04
remove <crashtesteam@gmail.com>
2014-04-09 15:15:59 +0300

    Sign in/Register

--- EntryApp/config/routes.rb
+++ EntryApp/config/routes.rb
@@ -4,7 +4,9 @@ EntryApp::Application.routes.draw do
   match '/', to: 'static_pages#home', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
-  match '/signup', to: 'clients#new', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'get'
 
   resources :attempt_answers
 
@@ -27,6 +29,8 @@ EntryApp::Application.routes.draw do
   resources :roles
 
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
