b3dda481eff6560abb3925f843907252c4d04612
Zumrad <zumrad89@mail.ru>
2014-04-09 15:00:07 +0300

    initial test&timetable form - unfinished

--- EntryApp/app/controllers/tests_controller.rb
+++ EntryApp/app/controllers/tests_controller.rb
@@ -25,9 +25,12 @@ class TestsController < ApplicationController
   # POST /tests.json
   def create
     @test = Test.new(test_params)
-
+   logger.debug params[:timetable]
     respond_to do |format|
       if @test.save
+         # @test.id new test id 
+        @timetable = Timetable.create(timetable_params)
+
         format.html { redirect_to @test, notice: 'Test was successfully created.' }
         format.json { render action: 'show', status: :created, location: @test }
       else
@@ -40,14 +43,30 @@ class TestsController < ApplicationController
   # PATCH/PUT /tests/1
   # PATCH/PUT /tests/1.json
   def update
+     logger.debug params[:timetable]
     respond_to do |format|
       if @test.update(test_params)
+
+        timetable_manage
+
         format.html { redirect_to @test, notice: 'Test was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
         format.json { render json: @test.errors, status: :unprocessable_entity }
       end
+    end
+  end
+# inset OR update timetable
+  def timetable_manage
+    # TODO add support for multiple timeslots
+    params[:timetable] = params[:timetable].merge(:test_id=> params[:id])
+    logger.debug "IMPORTANT INFO HERE!"
+    logger.debug params[:timetable]
+    if params[:timetable][:id] then
+       @timetable = Timetable.update(params[:timetable][:id], timetable_params) 
+    else 
+       @timetable = Timetable.create(timetable_params) 
     end
   end
 
@@ -70,5 +89,10 @@ class TestsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def test_params
       params.require(:test).permit(:title, :duration)
+    end
+
+     # Never trust parameters from the scary internet, only allow the white list through.
+    def timetable_params
+      params.require(:timetable).permit(:start, :end, :slots, :location, :test_id)
     end
 end
